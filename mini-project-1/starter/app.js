3fc2d7dc84b7cd979148e0595498d2544ee107e2
abdulgonimurod1 <abdulgonimurod1@gmail.com>
2023-10-24 14:43:55 +0700

    Add JS File, Change Title, Format code, fixing bug

---
+++ mini-project-1/starter/app.js
@@ -0,0 +1,29 @@
+const projectTitle = document.querySelector('.heading-title');
+projectTitle.textContent = "Title Baru dari JS File";
+
+var thisIsFunction = "function";
+
+const PI = 3.14;
+
+var y = 2;
+var z = 5;
+
+var x = y + z;
+
+function sumNum(a, b) { 
+    return a + b;
+}
+
+var iniSebuahVar = "Variable";
+
+iniSebuahVar = "Variable Baru";
+
+console.log(iniSebuahVar);
+
+// Ini Komen satu baris
+
+/**
+ Ini adalah komen 
+ lebih dari 
+ satu baris.
+*/
