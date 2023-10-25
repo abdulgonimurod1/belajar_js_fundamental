87af1091324d25a3535af5c852bbdaf71d45f048
abdulgonimurod1 <abdulgonimurod1@gmail.com>
2023-10-25 14:00:03 +0700

    var, let, and const

--- mini-project-1/starter/app.js
+++ mini-project-1/starter/app.js
@@ -1,24 +1,48 @@
+// var fullName = " Abdul Goni Murod";
+// var greeting  = "Hello";
+
+// console.log(greeting + fullName);
+
+// var and let
+
+// hoisting
+// console.log(bar);
+
+// let userName = "Goni";
+
+// var foo = "bar";
+
+// const bar = "foo";
+
+// scope
+
+// for (let i = 0; i < 4; i++) {
+//     console.log("hello")
+// }
+
+// console.log(i);
+
 const projectTitle = document.querySelector('.heading-title');
 projectTitle.textContent = "Title Baru dari JS File";
 
-var thisIsFunction = "function";
+// var thisIsFunction = "function";
 
-const PI = 3.14;
+// const PI = 3.14;
 
-var y = 2;
-var z = 5;
+// var y = 2;
+// var z = 5;
 
-var x = y + z;
+// var x = y + z;
 
-function sumNum(a, b) { 
-    return a + b;
-}
+// function sumNum(a, b) { 
+//     return a + b;
+// }
 
-var iniSebuahVar = "Variable";
+// var iniSebuahVar = "Variable";
 
-iniSebuahVar = "Variable Baru";
+// iniSebuahVar = "Variable Baru";
 
-console.log(iniSebuahVar);
+// console.log(iniSebuahVar);
 
 // Ini Komen satu baris
 
