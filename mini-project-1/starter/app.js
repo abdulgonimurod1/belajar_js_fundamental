77abc3246006973452bd795f62e4d09ccf3e44d8
abdulgonimurod1 <abdulgonimurod1@gmail.com>
2023-10-26 13:56:00 +0700

    values, Type Data and Operator Arithmatics

--- mini-project-1/starter/app.js
+++ mini-project-1/starter/app.js
@@ -51,3 +51,138 @@ projectTitle.textContent = "Title Baru dari JS File";
  lebih dari 
  satu baris.
 */
+
+// Type data
+/** Number
+
+let number = 2020;
+let numberFrictional = 20.19;
+
+ let veryBigNumber = 1.889e6 // 188.900.000
+*/
+
+/** Aritmatik Operator
+    
+ 
+
+let additional = 1000 + 500;
+
+let substraction = 1000 - 500;
+
+let multiplication = 100 * 5;
+
+let division = 100 / 5;
+
+// Presedence atau order of arithmetics
+
+let edgeCase = 100 + (4*2);
+
+// modulus atau remainder = %
+
+let modulus = 320 % 100;
+
+let even = 27 % 4;
+
+*/
+
+/** Special Numbers
+ 
+ let specialNum1 = Infinity;
+ 
+ let specialNum2 = -Infinity;
+ 
+ let specialCase = Infinity - 1;
+ 
+ let specialNum3 = NaN;
+ 
+ let specialCase2 = "five" - 0;
+ 
+ */
+
+/** 
+//  String
+
+let string1 = "Ini adalah string";
+let string2 = 'Ini adalah string2';
+let string3 = `Ini adalah string3`;
+
+let multilineString = "Ini adalah paragraph 1\nDan ini adalah paragraph 2"
+let multilineString2 = `Ini adalah paragraph 1 dengan backtic
+Dan ini adalah paragraph 2
+`
+
+let firstName = "Bambang";
+let lastName = "Pamungkas";
+let shirtNumber = 20;
+ 
+// String Concatenates
+let namaLengkap = firstName + ' ' + lastName
+
+let namaLengkap2 = `${firstName} ${lastName} ${shirtNumber}`;
+*/
+
+/**
+Type conversion or coercion
+
+console.log("100" - 50);
+
+console.log("100" + 50);
+
+console.log(8 * null);
+
+console.log('five' * 4);
+
+*/
+
+/** 
+// Boolean
+
+let yes = true;
+let no = false;
+
+// Comparison
+
+// console.log(2>=2)
+
+console.log("Apple" == "Orange")
+
+*/
+
+/** 
+// Logical operators and advanced comparison
+
+let firstName = "Abdul";
+let shirtNumber = 8
+
+// console.log(firstName == "Abdul") // Loose comparison
+// console.log(shirtNumber === "8") // Strict comparison
+
+
+// console.log(true && false); // &&
+
+
+// console.log(true || false) // ||
+
+let yes = !false;
+
+console.log(yes) // !
+
+*/
+
+// Array
+
+let makananFavorit = ["Mie Ayam", "Seblak", "Bakso"];
+
+// Objects
+
+let biodataObject = {
+    firstName: "Abdul",
+    lastName: "Goni Murod",
+    makananFavorit: ["Mie Ayam", "Seblak", "Bakso"],
+    istri: {
+        firstName: "no Name",
+        lastName: "infinity"
+    }
+}
+
+console.log(biodataObject);
